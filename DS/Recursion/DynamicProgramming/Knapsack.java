519f60fbbf9713ff8198c980a255453518b7d5f1
ravi26067 <ravi26067@gmail.com>
2020-06-12 02:19:15 +0530

    DP: 0-1 Knapsack problem

--- DS/Recursion/DynamicProgramming/Knapsack.java
+++ DS/Recursion/DynamicProgramming/Knapsack.java
@@ -1,10 +1,57 @@
 package data_structures.graph;
 
 /**
- * Iterative approach to solve knapsack problem. 
+ * Iterative and recursive apporach approach to solve knapsack problem. 
  * ks(w,i) = Max((value(i)+ks(W-w[i],i-1)),ks(W,i-1))
  */
 public class Knapsack {
+	
+	
+	
+	
+	public static int ksRec(int weight[],int item[],int W , int i) {
+		if(i==-1||W==0)
+			return 0;
+		
+		if(weight[i]<=W) {
+			int include = item[i] + ksRec(weight, item, W - weight[i], i-1);
+			int exclude = ksRec(weight, item, W , i-1);
+			return Math.max(include, exclude);
+		}
+		else {
+			return ksRec(weight, item, W , i-1);
+		}
+		
+	}
+	
+	
+	public static int ksMemoization(int weight[],int item[],int W , int i,int dp[][]) {
+		if(i==-1||W==0)
+			return 0;
+		
+		if(dp[W][i]!=-1)
+			return dp[W][i];
+		
+		if(weight[i]<=W) {
+			int include = item[i] + ksMemoization(weight, item, W - weight[i], i-1,dp);
+			int exclude = ksMemoization(weight, item, W , i-1,dp);
+			dp[W][i] = Math.max(include, exclude);
+			return dp[W][i];
+		}
+		else {
+			return dp[W][i] = ksMemoization(weight, item, W , i-1,dp);
+		}
+		
+	}
+	public static int knapSackMem(int weight[],int item[],int W ) {
+		int n =weight.length;
+		int dp[][] = new int[W+1][n];
+		for(int i=0;i<=W;i++) {
+			for(int j=0;j<n;j++)
+				dp[i][j]= -1;
+		}
+		return ksMemoization(weight, item, W, n-1, dp);
+	}
 
 	public static int knapsack(int weight[], int item[], int W) {
 		int n = item.length;
@@ -13,7 +60,6 @@ public class Knapsack {
 			for (int w = 1; w <= W; w++) {
 				if (weight[i - 1] <= w) {
 					dp[w][i] = Math.max(dp[w - weight[i - 1]][i - 1] + item[i - 1], dp[w][i - 1]);
-					System.out.println("dp [j][i] " + dp[w][i]);
 				} else {
 					dp[w][i] = dp[w][i - 1];
 				}
@@ -27,7 +73,8 @@ public class Knapsack {
 		int w[] = { 3, 7, 10, 6 };
 		int item[] = { 4, 14, 10, 5 };
 		int W = 20;
-		System.out.println(knapsack(w, item, W));
+		System.out.println("Using tabulation: "+ knapsack(w, item, W));
+		System.out.println("Using memoization: "+knapSackMem(w, item, W));
 	}
 
 }
