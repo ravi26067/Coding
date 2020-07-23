ba3944b3060096374ac961ecb2fe70b2da6dfcf4
Ravi Kumar <ravi26067@gmail.com>
2020-07-23 09:59:52 +0530

    Day 22 : Binary Tree Zigzag Level Order Traversal

--- coding/Leetcode30days/July2020/LeetcodeDay22.java
+++ coding/Leetcode30days/July2020/LeetcodeDay22.java
@@ -12,6 +12,46 @@
  *         this.right = right;
  *     }
  * }
+ 
+ public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		List<List<Integer>> ans = new LinkedList();
+		Stack<TreeNode>st1  = new Stack();
+		Stack<TreeNode>st2  = new Stack();
+		if(root==null)
+			return ans;
+		st1.add(root);
+		LinkedList<Integer> tmp = null;
+		while(true) {
+			tmp = new LinkedList<Integer>();
+			while(!st1.isEmpty()) {
+				root = st1.pop();
+				tmp.add(root.val);
+				if(root.left!=null)
+					st2.push(root.left);
+				if(root.right!=null)
+					st2.push(root.right);
+			}
+			ans.add(tmp);
+			tmp = new LinkedList<Integer>();
+			
+			while(!st2.isEmpty()) {
+				root = st2.pop();
+				tmp.add(root.val);
+				if(root.right!=null)
+					st1.push(root.right);
+				if(root.left!=null)
+					st1.push(root.left);
+			}
+			
+			ans.add(tmp);
+			tmp = new LinkedList<Integer>();
+			
+			if(st1.isEmpty() && st2.isEmpty())
+				break;
+		}
+		return ans;
+	}
+ 
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
