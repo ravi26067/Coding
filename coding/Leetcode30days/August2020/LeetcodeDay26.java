404b42409c10414603e69104e879cbcbc7c568b5
Ravi Kumar <ravi26067@gmail.com>
2020-08-26 21:04:37 +0530

    Day26: Fizz Buzz

---
+++ coding/Leetcode30days/August2020/LeetcodeDay26.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList();
+        String s = "";
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                ans.add("FizzBuzz");
+            }else if(i%3==0){
+                ans.add("Fizz");
+            }else if(i%5==0){
+                ans.add("Buzz");
+            }else{
+                s = String.valueOf(i); 
+                ans.add(s);
+            }
+        }
+        return ans;
+    }
+}
