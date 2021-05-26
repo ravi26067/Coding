7efe1c86a0855ad32ad5e5b6988a0db92b60db7c
Ravi Kumar <ravi26067@gmail.com>
2021-05-26 09:20:45 +0530

    Evaluate reverse polish notation

---
+++ Java/Strings/Leetcode/ReversePolish.java
@@ -0,0 +1,38 @@
+class Solution {
+    
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack();
+        int tmp = 0;
+        if(tokens.length == 1)
+        {
+            return Integer.parseInt(tokens[0]);
+        }
+        for(int i=0;i<tokens.length;i++){
+            String c = tokens[i];
+            if(c.length()==1 && (c.equals("+") || c.equals("-") || c.equals("/")||c.equals("*"))){
+                Integer op2 = st.pop();
+                Integer op1 = st.pop();
+                switch (c){
+                    case "+":
+                        tmp = op1 + op2;
+                        break;
+                    case "-":
+                        tmp = op1 - op2;
+                        break;
+                    case "/":
+                        tmp = op1 / op2;
+                        break;
+                    case "*":
+                        tmp = op1 * op2;
+                        break;
+                }
+                //if(!st.isEmpty())
+                    st.push(tmp);
+            }else{
+                Integer value = Integer.parseInt(c);
+                st.push(value);
+            }
+        }
+        return tmp;
+    }
+}
