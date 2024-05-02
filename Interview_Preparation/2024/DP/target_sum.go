2c054f30b937b9e727cee4adf3d1d847948fb134
Ravi Kumar <ravi26067@gmail.com>
2024-05-02 15:26:12 +0530

    Target Sum problem

---
+++ Interview_Preparation/2024/DP/target_sum.go
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/target-sum/
+func findTargetSumWays(nums []int, target int) int {
+    
+    n:= len(nums)
+    
+    dp:= make([][]int,n+1)
+
+    if target<0{
+        target *= -1
+    }
+
+
+    sum:=0
+
+    for _,val:=range(nums){
+        sum+=val
+    }
+    
+    if (sum+target)%2!=0{
+        return 0
+    }
+
+    s1:= (sum+target)/2
+
+    for i:=0;i<n+1;i++{
+        dp[i]=make([]int,s1+1)
+    }
+
+    dp[0][0] = 1
+    
+    
+    for i:=1;i<n+1;i++{
+        for j:=0;j<s1+1;j++{
+            if nums[i-1]<=j{
+                dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j]
+            }else{
+                dp[i][j] = dp[i-1][j]
+            }
+        }
+    }
+
+    return dp[n][s1]
+}
