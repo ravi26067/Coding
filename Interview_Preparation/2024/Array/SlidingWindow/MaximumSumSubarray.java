eb722fa357c8a379e3e8862f9489a069e91d87f2
Ravi Kumar <ravi26067@gmail.com>
2024-05-28 11:16:48 +0530

    maximumsa

--- Interview_Preparation/2024/Array/SlidingWindow/MaximumSumSubarray.java
+++ Interview_Preparation/2024/Array/SlidingWindow/MaximumSumSubarray.java
@@ -5,21 +5,45 @@ public class MaximumSumSubarray {
     static long maximumSumSubarray(int K, ArrayList<Integer> Arr, int N) {
         long sum = 0;
         long maxSum = 0;
-        for (int i = 0; i < K; i++) {
-            sum += Arr.get(i);
-        }
-        // System.out.println("Sum:"+sum);
-        maxSum = sum;
-        for (int i = 1; i <= N - K; i++) {
+        int i = 0;
+        int j = 0;
+        while (j < N) {
+            sum += Arr.get(j);
 
-            sum = sum - Arr.get(i - 1);
-            sum = sum + Arr.get(i + K - 1);
-
-            // System.out.println("Sum:"+sum);
-            if (sum > maxSum) {
-                maxSum = sum;
+            if (j - i + 1 < K) {
+                j++;
+            } else if (j - i + 1 == K) {
+                maxSum = Math.max(maxSum, sum);
+                sum -= Arr.get(i);
+                ;
+                i++;
+                j++;
             }
         }
         return maxSum;
     }
+
+    /*
+     * static long maximumSumSubarray(int K, ArrayList<Integer> Arr, int N) {
+     * long sum = 0;
+     * long maxSum = 0;
+     * for (int i = 0; i < K; i++) {
+     * sum += Arr.get(i);
+     * }
+     * // System.out.println("Sum:"+sum);
+     * maxSum = sum;
+     * for (int i = 1; i <= N - K; i++) {
+     * 
+     * sum = sum - Arr.get(i - 1);
+     * sum = sum + Arr.get(i + K - 1);
+     * 
+     * // System.out.println("Sum:"+sum);
+     * if (sum > maxSum) {
+     * maxSum = sum;
+     * }
+     * }
+     * return maxSum;
+     * }
+     */
+
 }
